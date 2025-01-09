2c58827674f6db9fb77edfa1aff97e1e8d500d38
nshutiyvonne1@gmail.com <ntwalidavid90@gmail.com>
2025-01-09 07:47:39 +0200

    updated some changes

--- src/components/Benefit.jsx
+++ src/components/Benefit.jsx
@@ -1,9 +1,5 @@
-import React from 'react'
-
-const Benefit = () => {
+export const GradientLight = () => {
   return (
-    <div>Benefit</div>
+    <div className="absolute top-0 left-1/4 w-full aspect-square bg-radial-gradient from-[#28206C] to-[#28206C]/0 to-70% pointer-events-none" />
   )
 }
-
-export default Benefit
