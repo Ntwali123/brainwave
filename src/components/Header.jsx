1adf6de7344790ff0926245b071a8189bca0800c
nshutiyvonne1@gmail.com <ntwalidavid90@gmail.com>
2025-01-11 10:46:58 +0200

    made some changes on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,12 @@
-import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { disablePageScroll, enablePageScroll } from "scroll-lock";
+
+import { brainwave } from "../assets";
+import { navigation } from "../constants";
+import Button from "./Button";
+import MenuSvg from "../assets/svg/MenuSvg";
+import { HamburgerMenu } from "./design/Header";
+import { useState } from "react";
 
 const Header = () => {
   const pathname = useLocation();
@@ -22,6 +28,7 @@ const Header = () => {
     enablePageScroll();
     setOpenNavigation(false);
   };
+
   return (
     <div
       className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
